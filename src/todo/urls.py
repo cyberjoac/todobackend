02586c0f0f370b729c07ab2640c1f3110be74c1a
Joachim Hagege <cyberjoac@gmail.com>
2016-09-12 23:48:35 +0300

    Docker

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -8,5 +8,5 @@ router.register(r'todos', views.TodoItemViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    url(r'^', include(router.urls))
+    url(r'^', include(router.urls)),
 ]
